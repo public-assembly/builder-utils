08699e88493d1a6c320f558d0cddc8def4a93029
Dain Blodorn Kim <dain@db13.us>
2022-12-16 18:32:11 -0800

    add gql-tag dep

--- apps/next/components/AppWrapper.tsx
+++ apps/next/components/AppWrapper.tsx
@@ -4,6 +4,7 @@ import { getDefaultWallets, RainbowKitProvider, lightTheme } from '@rainbow-me/r
 import { createClient, chain, configureChains, WagmiConfig } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { infuraProvider } from 'wagmi/providers/infura'
 import { SWRConfig } from 'swr'
 import { NFTFetchConfiguration } from '@zoralabs/nft-hooks'
 import { ZDKFetchStrategy } from '@zoralabs/nft-hooks/dist/strategies'
@@ -13,14 +14,18 @@ import { Footer } from './Footer'
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.goerli],
   [
-    jsonRpcProvider({
+    infuraProvider({
       priority: 0,
+      apiKey: process.env.NEXT_PUBLIC_INFURA_KEY,
+    }),
+    jsonRpcProvider({
+      priority: 1,
       rpc: (chain) =>
         chain.id === 1
           ? { http: 'https://rpc.ankr.com/eth' }
           : { http: 'https://rpc.ankr.com/eth_goerli' },
     }),
-    publicProvider(),
+    publicProvider({ priority: 2 }),
   ]
 )
 
