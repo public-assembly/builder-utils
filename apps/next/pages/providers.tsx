9f7778715830325da5348f4f67ccceb852d8cc81
Dain Blodorn Kim <dain@db13.us>
2023-01-18 09:32:35 -0800

    add test page to check next build

---
+++ apps/next/pages/providers.tsx
@@ -0,0 +1,12 @@
+import { Seo } from '@/components/Seo'
+
+function Providers() {
+  return (
+    <section className="max-w-[1240px] m-auto px-4 gap-8 flex flex-col">
+      <Seo title="Providers" />
+      <h1>Providers Examples</h1>
+    </section>
+  )
+}
+
+export default Providers
