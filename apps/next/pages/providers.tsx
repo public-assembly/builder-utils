0596f033aca3e09a13f82b4e62ebc18d86c04844
Dain Blodorn Kim <dain@db13.us>
2023-01-18 09:40:44 -0800

    comment out dao-utils imports in providers page

--- apps/next/pages/providers.tsx
+++ apps/next/pages/providers.tsx
@@ -1,15 +1,17 @@
 import { Seo } from '@/components/Seo'
-import { ManagerProvider } from '@public-assembly/dao-utils'
-import { PrintManagerProviderData } from '../components'
+// import { ManagerProvider } from '@public-assembly/dao-utils'
+// import { PrintManagerProviderData } from '../components'
 
 function Providers() {
   return (
     <section className="max-w-[1240px] m-auto px-4 gap-8 flex flex-col">
       <Seo title="providers" />
       <div className="bg-slate-300 p-4 rounded-2xl text-black">
+        {/*
         <ManagerProvider tokenAddress="0xdf9b7d26c8fc806b1ae6273684556761ff02d422">
           <PrintManagerProviderData />
         </ManagerProvider>
+         */}
       </div>
     </section>
   )
