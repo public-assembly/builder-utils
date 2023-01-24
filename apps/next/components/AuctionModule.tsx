500049a52af6c83c3ecdd33772b9b2e3e34b0444
Salief Lewis <salieflewis@gmail.com>
2023-01-24 17:23:09 -0500

    updates auction module to new typings

--- apps/next/components/AuctionModule.tsx
+++ apps/next/components/AuctionModule.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TokenExplorer } from '@public-assembly/dao-utils'
 
-export default function AuctionModule({ address }: { address: string }) {
+export default function AuctionModule({ address }: { address: `0x${string}` }) {
   return <TokenExplorer tokenAddress={address} />
 }
