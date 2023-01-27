e47bdf5d63c4f82e1923881c1b84c21a39d693b1
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-01-27 15:33:40 -0500

    fix: rendering with serverside check

--- apps/next/pages/providers.tsx
+++ apps/next/pages/providers.tsx
@@ -1,13 +1,15 @@
 import { Seo } from '@/components/Seo'
 import { ManagerProvider } from '@public-assembly/dao-utils'
+import { isServerSide } from 'utils/helpers'
 import { PrintManagerProviderData } from '../components'
 
 function Providers() {
   const tokenAddress = '0x8983ec4b57dbebe8944af8d4f9d3adbafea5b9f1' as `0x${string}`
+  if (isServerSide()) return null
   return (
     <section className="max-w-[1240px] m-auto px-4 gap-8 flex flex-col">
       <Seo title="providers" />
-      <div className="bg-slate-300 p-4 rounded-2xl text-black">
+      <div className="p-4 text-black bg-slate-300 rounded-2xl">
         <ManagerProvider tokenAddress={tokenAddress}>
           <PrintManagerProviderData />
         </ManagerProvider>
