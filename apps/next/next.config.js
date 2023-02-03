a78f69d57bbe6d1ea177a084d35998450b020672
Salief Lewis <salieflewis@gmail.com>
2023-02-03 10:44:50 -0500

    remove webpack config for markdown

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -5,19 +5,19 @@ const withTM = require('next-transpile-modules')(['@public-assembly/dao-utils'])
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(md|mdx)?$/,
-      use: [
-        {
-          loader: '@mdx-js/loader',
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {},
-        },
-      ],
-    })
-    return config
-  },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(md|mdx)?$/,
+  //     use: [
+  //       {
+  //         loader: '@mdx-js/loader',
+  //         /** @type {import('@mdx-js/loader').Options} */
+  //         options: {},
+  //       },
+  //     ],
+  //   })
+  //   return config
+  // },
 }
 
 module.exports = withTM(nextConfig)
