8bacd4fe99d026ffd5c013e58a12ff6ee6f1461c
Salief Lewis <salieflewis@gmail.com>
2023-02-17 17:23:30 -0500

    edit next/app next config

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
 
 const withTM = require('next-transpile-modules')(['@public-assembly/dao-utils'])
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(md|mdx)?$/,
+      use: [
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {},
+        },
+      ],
+    })
+    return config
+  },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     esmExternals: 'loose',
   },
 }
 
-module.exports = withMDX(withTM(nextConfig))
+module.exports = withTM(nextConfig)
