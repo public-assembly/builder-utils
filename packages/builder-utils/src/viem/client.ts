a742f587ade4d69d154bea17ceca986167d64ced
Salief <salieflewis@gmail.com>
2023-05-12 15:25:26 -0400

    restore to client

--- packages/builder-utils/src/viem/client.ts
+++ packages/builder-utils/src/viem/client.ts
@@ -1,11 +1,13 @@
-import * as React from 'react'
-import { createPublicClient, http, Transport } from 'viem'
+import { createPublicClient, Transport } from 'viem'
 import { CHAIN } from './viemChain'
 
-export const viemClient = createPublicClient({
+const transport =
+  `http://${CHAIN}.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}` as unknown as Transport
+
+export const client = createPublicClient({
   batch: {
     multicall: true,
   },
   chain: CHAIN,
-  transport: http(process.env.NEXT_PUBLIC_ALCHEMY_ENDPOINT),
+  transport,
 })
