5c51dd2688cb8f97ed5208297185d783f9be534a
0xTranqui <thinkandchill11@gmail.com>
2023-08-08 15:47:20 +0200

    added zora main + base support

--- packages/builder-utils/src/subgraph/subgraph.ts
+++ packages/builder-utils/src/subgraph/subgraph.ts
@@ -2,4 +2,7 @@ export const PUBLIC_SUBGRAPH_URL = {
   1: 'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-mainnet',
   5: 'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-goerli',
   999: 'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-zora-testnet/stable/gn',
+  7777777:
+    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-zora-mainnet/stable/gn',
+  8453: 'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-base-mainnet/stable/gn',
 }[process.env.NEXT_PUBLIC_CHAIN_ID || 1] as string
